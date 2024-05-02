604147ae89237b9b772c22efcad1f81644a8ccca
s27540 <s27540@pjwstk.edu.pl>
2024-05-02 11:01:36 +0200

    Dodanie modeli

---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class Product
+{
+    public int IdProduct { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Decimal Price { get; set; }
+}
---
+++ WebApplication1/WebApplication1/Models/ProductWarehouse.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models;
+
+public class ProductWarehouse
+{
+    public int IdProductwarehouse { get; set; }
+    public int IdWarehouse { get; set; }
+    public int IdProduct { get; set; }
+    public int IdOrder { get; set; }
+    public int Amount { get; set; }
+    public Decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+}
